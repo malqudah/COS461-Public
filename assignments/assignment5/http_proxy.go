e04b2d0312519422bfdef689bce35c114a5141ba
Mohammad Alqudah <malqudah@princeton.edu>
2021-11-10 18:08:45 -0500

    handleConnection

--- assignments/assignment5/http_proxy.go
+++ assignments/assignment5/http_proxy.go
@@ -1,8 +1,66 @@
 /*****************************************************************************
  * http_proxy.go                                                                 
- * Names: 
- * NetIds:
+ * Names: Mohammad Alqudah, Jonathan Salama
+ * NetIds: malqudah, jjsalama
  *****************************************************************************/
 
  // TODO: implement an HTTP proxy
  
+ package main
+
+ import (
+	 "net"
+	 "fmt"
+	 "os"
+	 "log"
+	 "bufio"
+	 "net/http"
+ )
+
+ func handleConnection(conn net.Conn) {
+	
+	reader := bufio.NewReader(conn)
+	request, err := http.ReadRequest(reader)
+
+	// come back here
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if request.Method != "GET" {
+		newResponse := []byte("HTTP 500 Internal Error")
+		conn.Write(newResponse)
+		return
+	}
+
+	
+
+ }
+
+ func main() {
+
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: ./server-go [server port]")
+	  }
+	server_port := os.Args[1]  
+
+	ln, err := net.Listen("tcp", server_port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Listening on port %s", server_port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+
+ }
